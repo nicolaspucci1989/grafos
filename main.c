01124e8885512422dd7ad337455b5678415e71cf
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-02-11 23:44:20 -0300

    primera implementacion de bfs en main

--- main.c
+++ main.c
@@ -2,40 +2,57 @@
 #include <stdlib.h>
 #include "grafos.h"
 
+
 int main() {
-  Nodo *n, *m, *v, *x;
-  Fifo *f = NULL;
-
-  f = newFifo(f);
-  f->final = f->cabeza = 0;
-  n = newNodo();
-  v = newNodo();
-  x = newNodo();
-  inicalizar_nodo(v);
-  inicalizar_nodo(x);
-  inicalizar_nodo(n);
-  n->id = 1;
-  x->id = 2;
-  v->id = 3;
-  push(f, n);
-  push(f, x);
-  push(f, v);
-  imprimirFifo(f);
-  puts("==================");
-
-  // imprimirFifo(f);
-  m = pop(f);
-  imprimir_nodo(m);
-  m = pop(f);
-  imprimir_nodo(m);
-  m = pop(f);
-  imprimir_nodo(m);
+  // int vecinos[VECINOS_MAX];
+  int i;
+  Nodo *m = NULL, *n = NULL;
+  Nodo grafo[NODOS_MAX];
+  Fifo fifo;
 
 
-  free(n);
-  free(x);
-  free(v);
-  free(f);
+  inicializar_fifo(&fifo);
+  inicializar_grafo(grafo);
 
+  grafo[0].id = 0;
+  grafo[0].vecinos[0] = 1;
+  grafo[0].vecinos[1] = 2;
+  grafo[1].id = 1;
+  grafo[1].vecinos[0] = 0;
+  grafo[1].vecinos[1] = 3;
+  grafo[2].id = 2;
+  grafo[2].vecinos[0] = 0;
+  grafo[2].vecinos[1] = 3;
+  grafo[3].id = 3;
+  grafo[3].vecinos[0] = 1;
+  grafo[3].vecinos[1] = 2;
+
+  //push fuente 0
+  grafo[0].distancia = 0;
+  push(&fifo, &(grafo[0]));
+
+  while(!estaVacio(&fifo)){
+    m = pop(&fifo);
+    for(i=0; i<VECINOS_MAX; i++){
+      if( m->vecinos[i] != -1 && grafo[m->vecinos[i]].distancia == -1){
+        n = &(grafo[m->vecinos[i]]);
+        n->distancia = m->distancia + 1;
+        // n->predecesor = m;
+        push(&fifo, n);
+      }
+    }
+  }
+
+  imprimir_grafo(grafo);
+
+
+  // if(!estaVacio(&fifo)){
+  //   m = pop(&fifo);
+  //   n = &(grafo[m->vecinos[0]]);
+  //   n->distancia = m->distancia + 1;
+  //   n->predecesor = m;
+  //   imprimir_nodo(n);
+  // }
+  //
   return 0;
 }
